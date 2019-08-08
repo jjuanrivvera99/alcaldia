699d9051c3559a148ac2a190ab6a0f945fa9f3bd
Juan Felipe Rivera González <jjuanrivvera@gmail.com>
2019-08-07 22:21:55 -0500

    Se agregaron los seeds al DatabaseSeeder

--- core/database/seeds/DatabaseSeeder.php
+++ core/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"');
+
         $this->call(UsersTableSeeder::class);
+        $this->call(TipoIdentificacionTableSeeder::class);
+        $this->call(JornadaTableSeeder::class);
+        $this->call(ModalidadTableSeeder::class);
+        $this->call(PaisTableSeeder::class);
+        $this->call(CiudadTableSeeder::class);
+
+        DB::statement('EXEC sp_msforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"');
     }
 }
