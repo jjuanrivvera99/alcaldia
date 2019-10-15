15c47cc2884045fa10ab1f9114fc567c1a4b08a3
Juan Felipe Rivera González <jjuanrivvera@gmail.com>
2019-10-15 15:30:49 -0500

    CRUD ciudad

--- core/app/Ciudad.php
+++ core/app/Ciudad.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ciudad extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "core.ciudad";
+
+    /**
+     * The primary key attribute.
+     *
+     * @var string
+     */
+    protected $primaryKey = "id_ciudad";
+
+    /**
+     * The attributes that are fillable via mass assignment.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_pais',
+        'nombre'
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
 }
---
+++ core/app/Http/Controllers/CiudadController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ciudad;
+use Illuminate\Http\Request;
+
+class CiudadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ciudades = Ciudad::all();
+
+        return response()->json($ciudades);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $ciudad = $request->all();
+
+        $created = Ciudad::create($ciudad);
+
+        return response()->json($created);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $ciudades = Ciudad::findOrFail($id);
+
+        return response()->json($ciudades);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $ciudad = Ciudad::findOrFail($id)->update($request->all());
+
+        return response()->json($ciudad);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $ciudad = Ciudad::findOrFail($id);
+
+        return response()->json($ciudad->delete());
+    }
+}
