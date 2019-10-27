e17028b0dcdd8419ec43df351ac9adf2ccd1ce68
Juan Felipe Rivera González <juan.rivera@alegra.com>
2019-10-27 00:29:11 -0500

    SQL Server Certificate

--- mssql/queries/certificate.sql
+++ mssql/queries/certificate.sql
@@ -3,3 +3,20 @@ GO
 -- create master key
 CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'A125277935*#2019@';
 GO
+
+CREATE CERTIFICATE SQLalcaldia
+    WITH SUBJECT = 'SQLalcaldia Backup Certificate';
+GO
+
+BACKUP MASTER KEY
+    TO FILE = '/var/opt/mssql/data/certificates/SQLalcaldia.key'
+    ENCRYPTION BY PASSWORD = 'A125277935*#2019@';
+GO
+
+BACKUP CERTIFICATE SQLalcaldia
+    TO FILE = '/var/opt/mssql/data/certificates/CERSQLalcaldia.cer'
+    WITH PRIVATE KEY
+    (
+        FILE = '/var/opt/mssql/data/certificates/SQLalcaldiaBackup.key', ENCRYPTION BY PASSWORD = 'A125277935*#2019@'
+    );
+GO
